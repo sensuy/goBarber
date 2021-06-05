33bf7a22a6cd66f27a7acb72be032d0845e4125d
sensui <sensuijunior@hotmail.com>
2021-06-05 19:52:45 -0300

    Bcrypt hash

--- src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 import User from '../../infra/typeorm/entities/User';
@@ -6,19 +8,31 @@ class UsersRepository implements IUsersRepository {
   private users: User[] = [];
 
   public async findById(id: string): Promise<User | undefined> {
-    return user;
+    const findUser = this.users.find((user) => user.id === id);
+    return findUser;
   }
 
   public async findByEmail(email: string): Promise<User | undefined> {
-    return user;
+    const findUser = this.users.find((user) => user.email === email);
+    return findUser;
   }
 
   public async create(userData: ICreateUserDTO): Promise<User> {
-    return appointment;
+    const user = new User();
+
+    Object.assign(user, { id: uuidv4() }, userData);
+
+    this.users.push(user);
+
+    return user;
   }
 
   public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex((findUser) => findUser.id === user.id);
 
+    this.users[findIndex] = user;
+
+    return user;
   }
 }
 
